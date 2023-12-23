5d9898e10df13e5d3cf2dbc63d4da903452c17e1
mudassir-arif <mudassir@aptechgdn.net>
2023-12-23 14:33:13 +0500

    hamburger is ready

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const bars = document.getElementById("bars");
 const cross = document.getElementById("cross");
+const menu = document.getElementById("menu");
 
 cross.style.display= "none";
+menu.style.top = "-100%";
 
 bars.addEventListener("click" , function(){
     cross.style.display= "block";
     bars.style.display= "none";
+    menu.style.transition = "all 1s ease 0s"
+    menu.style.top = "64px";
 })
 
 cross.addEventListener("click" , function(){
     bars.style.display= "block";
     cross.style.display= "none";
+    menu.style.top = "-100%";
 })
 
 
