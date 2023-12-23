1cf7c781244454d5e0e19250080eae5f4b3f6aac
mudassir-arif <mudassir@aptechgdn.net>
2023-12-23 15:00:01 +0500

    responsive navbar is ready

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ menu.style.top = "-100%";
 bars.addEventListener("click" , function(){
     cross.style.display= "block";
     bars.style.display= "none";
-    menu.style.transition = "all 1s ease 0s"
+    menu.style.transition = "all 0.5s ease 0s"
     menu.style.top = "64px";
 })
 
